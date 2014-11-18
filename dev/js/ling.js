2916727d3a13554c1dbf5b85359379f6342025ed
Liuchenling <wssgcg1213@qq.com>
2014-11-18 17:20:48 +0800

    add gitignore

--- dev/js/ling.js
+++ dev/js/ling.js
@@ -10,25 +10,27 @@
     bindScroll($('.find'), $(".m-sechand"));
     bindScroll($('.study-help'), $(".m-chairs"));
 
-    function bindSwitcher($slider, $tabs){
+    function bindSwitcher($slider, $tabs, $cover){
         $tabs.each(function(i, item){
             $(item).on('click', function(){
                 $slider.hide();
                 $tabs.removeClass('z-active');
                 $(this).addClass('z-active');
                 $($slider[i]).show();
+                $cover.css('top', 120 * i + 'px');
             });
+
         });
     }
     var firstSlider = $('.m-alir').find('.m-slider-con, .m-news-con, .m-teach-con');
     var firstSliderTab = $('.tab-hot, .tab-cqupt-news, .tab-teach-news');
-    bindSwitcher(firstSlider, firstSliderTab);
+    bindSwitcher(firstSlider, firstSliderTab, $('.m-alir .z-cover'));
 
-    var secondSlider = $('.m-origin').find('.m-slider-con, .m-novels, .m-films');
-    var secondSliderTab = $('.tab-album, .tab-novel, .tab-film');
-    bindSwitcher(secondSlider, secondSliderTab);
+    var secondSlider = $('.m-origin').find('.m-slider-con, .m-films, .m-novels');
+    var secondSliderTab = $('.tab-album, .tab-film, .tab-novel');
+    bindSwitcher(secondSlider, secondSliderTab, $('.m-origin .z-cover'));
 
     var thirdSlider = $('.m-sechand').find('.m-slider-con');
-    var thirdSliderTab = $('.tab-products, .tab-daily, .tab-daily');
-    bindSwitcher(thirdSlider, thirdSliderTab);
+    var thirdSliderTab = $('.tab-products, .tab-daily, .tab-learn');
+    bindSwitcher(thirdSlider, thirdSliderTab, $('.m-sechand .z-cover'));
 })(jQuery);
